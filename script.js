ac4503748c057efcfd1165eff0457665c339b529
FrancescoTormene <francesco.tormene@gmail.com>
2025-02-21 10:00:20 +0100

    animazione comparsa menu

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 function toggleMenu() {
     const header = document.querySelector('header');
     const menu = document.querySelector('menu');
+
     header.classList.toggle('active');
+    if (header.style.height === "0px" || !header.style.height) {
+        header.style.height = header.scrollHeight + "px";
+    } else {
+        header.style.height = "0px";
+    }
+
     menu.classList.toggle('active');
+    if (menu.style.height === "0px" || !menu.style.height) {
+        menu.style.height = menu.scrollHeight + "px";
+    } else {
+        menu.style.height = "0px";
+    }
 }
